2a6833c19749f7019b3721fc07dea07373db939e
RoccoCerro <rcerro02@gmail.com>
2024-03-01 12:40:19 +0100

    Esercizio completato

---
+++ js/app.js
@@ -0,0 +1,42 @@
+// Esercizio 1
+// Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
+
+const number = 100; //number
+
+console.log(number);
+
+// Esercizio 2
+// Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+
+const PI = 3.14; //number
+
+console.log(PI);
+
+// Esercizio 2/B
+// Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+
+// PI = 3.1416;
+
+// console.log(PI);
+    // Da un errore in console perchè il valore di una variabile const non può essere modificato
+
+// Esercizio 2/C
+// Dichiara una variabile chiamata radius e assegnale il valore 8.
+// Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+// Stampa il valore della variabile circle in console.
+
+const radius = 8; //number
+const circle = radius * 2 * PI; //number
+
+console.log(circle);
+
+// Esercizio 3
+// Dichiara una variabile chiamata name e
+// assegnale il valore “Carlo”.
+// Successivamente, modifica il valore della variabile in “Marco”.
+// Stampa il nuovo valore in console.
+
+let name = "Carlo"; //string
+name = "Marco"; //string
+
+console.log(name);
